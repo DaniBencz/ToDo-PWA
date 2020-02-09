e662e974e36c8d4efe815296adc128a254948ef0
Daniel <daniel.bencz@gmail.com>
2020-02-09 11:55:26 +0100

    caching .png on the fly

--- todo-sw.js
+++ todo-sw.js
@@ -1,14 +1,13 @@
 'use strict'
 
-console.log("in todo-sw.js")
+// console.log("in todo-sw.js")
 const cacheName = "todo2"
 
-/* window. */self.addEventListener('install', function (e) {
-  console.log('worker installed')
+/* window. */self.addEventListener('install', e => {
+  // console.log('worker installed')
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('caching')
-
+    caches.open(cacheName).then(cache => {
+     // console.log('caching')
       return cache.addAll([
         '/',  // this is needed, else won't work! 
         'favicon.ico',
@@ -17,13 +16,12 @@ const cacheName = "todo2"
         'main.js',
         'https://unpkg.com/primitive-ui/dist/css/main.css',
         'manifest.json',
-        // 'png/launcher-icon-144.png'
       ])
     }).then(() => self.skipWaiting())
   )
 })
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(cacheNames => {
       cacheNames.forEach(name => {
@@ -37,26 +35,26 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', e => {
 
-  if (e.request.url.includes('/launcher-icon')) {
-    console.log('.png!')
+  if (e.request.url.includes('/launcher-icon')) { // all this only runs after reload
     e.respondWith(
       caches.match(e.request) // try cache first
-        .then((resp) => {
+        .then(resp => {
+          // console.log(resp)
           return resp || fetch(e.request) // cache won't yield first time around
-            // .then() // use this to add .png files to cache
+            .then(f_resp => {
+              const respClone = f_resp.clone()  // resp can be used only once, workaround to clone it
+              caches.open(cacheName)
+                .then(cache => {
+                  // console.log('updating')
+                  cache.put(e.request, respClone)
+                })
+              return f_resp
+            })
         })
     )
   }
   else {
     e.respondWith(
-
-      // fetch(e.request) // do a regular fetch, no action
-      //   //.then(() => console.log('fetching: ', e.request.url))
-      //   .catch(() => { // there was network error, we use cache instead
-      //     console.log('loading from cache')
-      //     return caches.match(e.request)
-      //   })
-
       caches.match(e.request)
         .then((resp) => {
           return resp || fetch(e.request)
